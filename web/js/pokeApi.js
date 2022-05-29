79610aae2b51fab3e79533e5cc65f4dccf7c08ec
Anthony <amcaricola@gmail.com>
2022-05-29 17:37:13 -0400

    Pokemon API terminada, con buscador inteligente y detalles minimos corregidos

--- web/js/pokeApi.js
+++ web/js/pokeApi.js
@@ -19,14 +19,61 @@ const PokeSAtk = document.getElementById("pokemonSAtk")
 const pokeSDef = document.getElementById("pokemonSDef")
 const pokeSpeed = document.getElementById("pokemonSpeed")
 
+// Definicion de variable, array de nombres de pokemon para el buscador
+let pokemonName = document.getElementById("pokemonIDname");
+
+let pokemonsNamesArray = [];
+    check("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+    .then(data => {
+        data.results.forEach(item => 
+            pokemonsNamesArray.push(item.name)
+        )
+            
+    })
+    .catch(err => err)
+
+
+        
+// Definicion del imput y del buscador automatico 
+
+let pokemonResultado = document.querySelector('#resultadoBusqueda')
+
+pokemonName.addEventListener("keyup", (e)=> {
+    pokemonResultado.innerHTML = ""
+    let resultadosNombres = []
+    if(pokemonName.value === ""){
+        return 
+    } 
+    for (let name of pokemonsNamesArray){
+        if (name.toLowerCase().indexOf(pokemonName.value.toLowerCase()) > -1){
+            resultadosNombres.push(name)
+        }
+    };
+    for (let i = 0; i < 10; i ++ ){
+        if(resultadosNombres[i] === undefined){
+            return 
+        } 
+        if (pokemonName.value === resultadosNombres[i]){
+            return
+        }
+        pokemonResultado.innerHTML += `<li><a>${resultadosNombres[i]}</a></li>`
+    };
+})
+
+//Evento de click del buscador
+
+pokemonResultado.addEventListener("click", (e) => {
+    pokemonName.value = e.target.innerHTML
+    pokemonResultado.innerHTML = ""
+})
+
 
 // Definicion de Botton y de su accion.
 
 const buttonPokemon =  document.getElementById("pokemonButton")
 
 buttonPokemon.onclick = () => {
-
-    let pokemonName = document.getElementById("pokemonIDname");
+    
     let name = pokemonName.value || "pikachu";
     let api_url =  "https://pokeapi.co/api/v2/pokemon/"
     let fullUrl = api_url + name.toLowerCase() +"/"
@@ -34,13 +81,13 @@ buttonPokemon.onclick = () => {
 
     check(fullUrl)
         .then(data => {
-            console.log(data)
+            
 
 
             nombre.innerText = data.name
             id.innerText = data.id
-            alto.innerText = data.height
-            peso.innerText = data.weight
+            alto.innerText = `${parseFloat(data.height) / 10} m`
+            peso.innerText = `${parseFloat(data.weight) /10} Kg`
 
             
             let tipos = data.types.map(function(type){  
@@ -61,7 +108,6 @@ buttonPokemon.onclick = () => {
             pokeSpeed.innerText = statsbase[5]
 
             let imgPk = data.sprites.other["official-artwork"].front_default
-            console.log(imgPk)
 
             imagenpk.src = imgPk 
 
