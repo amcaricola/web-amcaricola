678d0f1996e8ee9551ddc28d8118f27ff6c2d3d0
Anthony <amcaricola@gmail.com>
2022-06-05 18:21:03 -0400

    se modifico la pagina inicial, permitiendo que la informacion se actualize instantaneamente con JS, se agrego el FAVICON, y se corrigieron informaciones menores

--- web/js/index.js
+++ web/js/index.js
@@ -1,5 +1,39 @@
-// const indexConstructor = document.querySelector('#autoConstruccion')
+//importacion de variables HTML
+import { proyecto, sobreMi, contacto } from "./Variables_html.js"
 
-// indexConstructor.innerHTML =`
 
-// `
+//Declaracion de variables
+// DOM
+
+const indexConstructor = document.querySelector('#autoConstruccion')
+
+
+// HTML NAV 
+const proyectoNav = document.querySelector("#proyectoNav")
+const sobreMiNav = document.querySelector("#sobreMiNav")
+const contactoNav = document.querySelector("#contactoNav")
+
+
+
+// Body que carga al inicio 
+indexConstructor.innerHTML = proyecto
+proyectoNav.style["font-weight"] = "bold"
+
+//Funcion modificadora del body
+function cambioBodyHtml(pagina, variable) {  
+    
+    proyectoNav.style["font-weight"] = "normal"
+    sobreMiNav.style["font-weight"] = "normal"
+    contactoNav.style["font-weight"] = "normal"
+  
+
+    indexConstructor.innerHTML = pagina
+    variable.style["font-weight"] = "bold"
+
+};
+
+
+proyectoNav.addEventListener("click", () => { cambioBodyHtml(proyecto,proyectoNav)})
+sobreMiNav.addEventListener("click", () => { cambioBodyHtml(sobreMi,sobreMiNav)})
+contactoNav.addEventListener("click", () => { cambioBodyHtml(contacto,contactoNav)})
+
